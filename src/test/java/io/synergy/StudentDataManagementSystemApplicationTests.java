2a7808fe4d349d50f7d5e3c40cc0045a8ed5e605
Ivan Mizinov <mizinov.ivan@yandex.ru>
2025-11-19 13:03:57 +0300

    Initial commit

---
+++ src/test/java/io/synergy/StudentDataManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.synergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
